dd374f58dee78c4d86800eded413949a3f874971
AndyMelR <63148426+AndyMelR@users.noreply.github.com>
2020-05-30 22:32:28 +0200

    Adding tests to app

---
+++ test_flask_app.py
@@ -0,0 +1,20 @@
+import unittest
+from test_base import TestFlaskBase
+
+class TestWeb(TestFlaskBase):
+    def test_server_is_on(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        print ("Hola")
+    
+    def test_route_index_is_Hola_mundo(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data,b'Hola, mundo')
+
+        
+if __name__ == '__main__':
+    unittest.main()
+
+
+
